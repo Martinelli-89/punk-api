7258d7f357a9caa57e050aa0ad3b74836621152d
Martinelli-89 <99336076+Martinelli-89@users.noreply.github.com>
2022-10-21 11:58:27 +0100

    main search param carousel working

--- src/Components/SearchMenu/SearchMenu.jsx
+++ src/Components/SearchMenu/SearchMenu.jsx
@@ -2,8 +2,27 @@ import {React, useState} from "react";
 import "./SearchMenu.scss";
 import dropUp from "../../Assets/Images/DropUp.svg";
 import dropDown from "../../Assets/Images/DropDown.svg"
+import Searchoptions from "../SearchOptions/SearchOptions";
 
 const SearchMenu = ({showMenu, toggleShowMenu}) => {
+
+    const [firstParams, setFirstParams] = useState([["ABV", ["LESSER THEN", "GREATER THEN"]], ["IBU", ["LESSER THEN", "GREATER THEN"]], ["EBC", ["LESSER THEN", "GREATER THEN"]], ["BY DATE", ["BREWED BEFORE", "BREWED AFTER"]], ["BY NAME", ["ENTER NAME BELOW"]],["BY YEAST", ["ENTER YEEAST BELOW"]],["BY HOP", ["ENTER HOP BELOW"]],["BY MALT", ["ENTER MALT BELOW"]]]);
+    
+    const handleMainParamChange = (event) => {
+
+        if (event.target.className == "left") {
+            const firstElem = [firstParams[0]];
+            const remainingParams = firstParams.slice(1);
+            const updatedParams = remainingParams.concat(firstElem);
+            setFirstParams(updatedParams);
+        } else {
+            const lastElem = [firstParams[firstParams.length-1]];
+            const remainingParams = firstParams.slice(0, -1);
+            const updatedParams = lastElem.concat(remainingParams);
+            setFirstParams(updatedParams);
+        }
+
+    }
 
     let menu;
 
@@ -20,7 +39,7 @@ const SearchMenu = ({showMenu, toggleShowMenu}) => {
         menu = 
             <div className="dropDown">
                 <img src={dropDown} alt="Close menu" className="dropDown__icon" onClick={toggleShowMenu}></img>
-                <p>Search option here</p>
+                <Searchoptions options={firstParams} swipeParams={handleMainParamChange} />
             </div>;
 
 
---
+++ src/Components/SearchOptions/SearchOptions.jsx
@@ -0,0 +1,32 @@
+import {React, useState} from "react";
+import "./SearchOption.scss";
+
+import left from "../../Assets/Images/left.svg";
+import right from "../../Assets/Images/right.svg";
+
+const SearchOptions = ({options, swipeParams}) => {
+
+    return (
+
+        <section className="searchParams">
+            <div className="mainParam">
+                <img src={left} alt="slideLeft" onClick={swipeParams} className="mainParam__left"></img>
+                <p className="mainParam__sideL">{options[0][0]}</p>
+                <p className="mainParam__middle">{options[1][0]}</p>
+                <p className="mainParam__sideR">{options[2][0]}</p>
+                <img src={right} alt="slideRight" onClick={swipeParams} className="mainParam__right"></img>
+            </div>
+            <div className="secondParam">
+                <img src={left} alt="secSlideLeft" className="secondParam__left"></img>
+                <p className="secondParam__param">{options[1][1][0]}</p>
+                <img src={right} alt="secSlideRight" className="secondParam__right"></img>
+            </div>
+            <input></input>
+            <button>See menu</button>
+        </section>
+
+    );
+
+}
+
+export default SearchOptions;
